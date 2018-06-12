3168e744695cd9a01ab849b6c6899bcd5293ead5
leonhardt <lariscusObscurus@gmail.com>
2018-06-12 14:25:00 +0200

    Missing stuff

--- main.cpp
+++ main.cpp
@@ -78,18 +78,31 @@ rgb hsv2rgb(hsv in)
     return out;
 }
 
+
+double sigmoid(int value)
+{
+    value -= 6.;
+    auto result = 1 / (std::exp(-1 * (value - 0.5)) + 1);
+    return result;
+}
+
 template<size_t width, size_t height>
 void ShowFractal(std::array<unsigned char, width * height> &result)
 {
+    auto minimum = *std::min_element(result.begin(), result.end());
+    auto maximum = *std::max_element(result.begin(), result.end());
+
     sf::Image image;
     image.create(width, height);
+
+#pragma omp parallel for collapse(2) schedule(dynamic)
     for (int i = 0; i < height; i++) {
         for (int j = 0; j < width; j++) {
             auto val = result.at(i * width + j);
             auto hsv_value = hsv{};
-            hsv_value.h = val;
+            hsv_value.h = ((360 / (maximum - minimum)) * (val - minimum) + 180) % 360;
             hsv_value.s = 1;
-            hsv_value.v = val;
+            hsv_value.v = sigmoid(val);
             auto rgb = hsv2rgb(hsv_value);
             image.setPixel(j, i, sf::Color(static_cast<sf::Uint8>(rgb.r * 255),
                                            static_cast<sf::Uint8>(rgb.g * 255),
@@ -97,7 +110,7 @@ void ShowFractal(std::array<unsigned char, width * height> &result)
         }
     }
 
-    image.saveToFile("test.bmp");
+    image.saveToFile("fractal.png");
     sf::Texture texture;
     texture.create(width, height);
     texture.update(image);
@@ -125,9 +138,12 @@ void ShowFractal(std::array<unsigned char, width * height> &result)
 
 int main()
 {
+    omp_set_num_threads(8);
     const int WIDTH = 1024;
     const int HEIGHT = 1024;
-    auto result = mandelbrot_set<WIDTH, HEIGHT>(-2.0, 0.5, -1.25, 1.25, 256);
+    const int ITERATIONS = 256;
+    auto result = mandelbrot_set<WIDTH, HEIGHT>(-2.0, 0.5, -1.25, 1.25, ITERATIONS); //Everything
+    //auto result = mandelbrot_set<WIDTH, HEIGHT>(-0.8, -0.7, 0, 0.1, ITERATIONS); //Zoomed
 
     ShowFractal<WIDTH, HEIGHT>(result);
 
--- src/Mandelbrot.h
+++ src/Mandelbrot.h
@@ -4,6 +4,8 @@
 #include <complex>
 #include <array>
 #include <vector>
+#include <chrono>
+#include <omp.h>
 
 template<typename T = double>
 std::vector<T> linspace(T minimum, T maximum, size_t N)
@@ -24,7 +26,7 @@ inline int mandelbrot(std::complex<double> c, int iterations)
 {
     auto z = c;
     for (int n = 0; n < iterations; n++) {
-        if (std::abs(z) > 2) {
+        if (z.real() * z.real() + z.imag() * z.imag() > 4) {
             return n;
         }
         z = z * z + c;
@@ -40,18 +42,20 @@ mandelbrot_set(double xmin, double xmax, double ymin, double ymax, int iteration
     auto vertical_values = linspace(ymin, ymax, height);
     auto image = std::array<unsigned char, height * width>{};
 
+    auto start_time =  std::chrono::steady_clock::now();
+
+#pragma omp parallel for collapse(2) schedule(dynamic)
     for (int row = 0; row < height; row++) {
         for (int col = 0; col < width; col++) {
             std::complex<double> c = 1i * vertical_values[row] + horizontal_values[col];
             auto required_iterations = mandelbrot(c, iterations);
-            try {
-                image.at(row * width + col) = required_iterations;
-            } catch (std::out_of_range) {
-               volatile int a = 0;
-            }
-
+            image[row * width + col] = required_iterations;
         }
     }
+
+    auto end_time =  std::chrono::steady_clock::now();
+    auto diff = end_time - start_time;
+    std::cout << std::chrono::duration <double, std::milli> (diff).count() <<  " ms" << std::endl;
     return image;
 }
 
